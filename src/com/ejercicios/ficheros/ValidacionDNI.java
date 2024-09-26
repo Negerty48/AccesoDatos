306161858fdfb797aa04ab396f3bbaeec4e247ef
Negerty48 <aangeltr48@gmail.com>
2024-09-26 18:07:53 +0200

    Subir validacion DNI

---
+++ src/com/ejercicios/ficheros/ValidacionDNI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ejercicios.ficheros;
+
+/*
+    A partir del archivo adjunto, que contiene una lista de números de 
+    identificación personal (DNI), desarrolla un programa que lea este archivo y
+    copie los DNIs correctamente formateados en un archivo llamado 'dniok.txt',
+    mientras que los DNIs incorrectamente formateados se guardarán en un archivo
+    denominado 'dnionotok.txt'. Un DNI se considera correctamente formateado si 
+    consta de 8 números seguidos de una letra, y la letra es válida según los 
+    estándares especificados en la siguiente página web.
+ */
+public class ValidacionDNI {
+    public static void main(String[] args) {
+        
+    }
+}
