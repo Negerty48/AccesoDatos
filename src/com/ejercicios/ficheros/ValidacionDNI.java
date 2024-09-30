6f9d2d15de264e193f6934db35fb889729c5f840
Negerty48 <aangeltr48@gmail.com>
2024-09-30 16:17:33 +0200

    Validacion DNI mas o menos hecho

--- src/com/ejercicios/ficheros/ValidacionDNI.java
+++ src/com/ejercicios/ficheros/ValidacionDNI.java
@@ -4,6 +4,11 @@
  */
 package com.ejercicios.ficheros;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 /*
     A partir del archivo adjunto, que contiene una lista de números de 
     identificación personal (DNI), desarrolla un programa que lea este archivo y
@@ -15,6 +20,67 @@ package com.ejercicios.ficheros;
  */
 public class ValidacionDNI {
     public static void main(String[] args) {
+        File fDni = new File("dnis.txt");
+        File fDniOk = new File("dniOk.txt");
+        File fDniNotOk = new File("dniNotOk.txt");
+        String linea;
         
+        try (BufferedReader bfr = new BufferedReader(new FileReader(fDni));) {
+            do {
+                linea = bfr.readLine();
+                if (linea != null) {
+                    if (comprobarFormato(linea)) {
+                        if (dniValido(linea)) {
+                            añadirFichero(linea, fDniOk);
+                        } else {
+                            añadirFichero(linea, fDniNotOk);
+                        }
+                    } else {
+                        añadirFichero(linea, fDniNotOk);
+                    }                
+                } 
+            } while (linea != null);            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static boolean comprobarFormato(String linea) {
+        boolean formato = false;
+        if (linea.length() == 9) {
+            for (int i = 0; i < linea.length(); i++) {
+                if (i != 8) {
+                    if (Character.isDigit(linea.charAt(i))) {
+                        formato = true;
+                    } else formato = false;                    
+                } else {
+                    if (Character.isAlphabetic(linea.charAt(i))) {
+                        formato = true;
+                    } else formato = false;
+                }
+            }
+        }
+        return formato;
+    }
+    
+    public static boolean dniValido(String linea) {        
+        int res;
+        int numero;
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+                        'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'}; 
+        numero = Integer.parseInt(linea.substring(0, 8));
+        res = numero % 23;
+        if (linea.charAt(7) == letras[res]) {
+            return true;
+        } else return false;        
+    }
+    
+    public static void añadirFichero(String linea, File f) {
+        try (BufferedWriter bfw = new BufferedWriter(new FileWriter(f, true));) {
+            bfw.write(linea + "\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
